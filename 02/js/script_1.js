eac734b315a8d23300912d6d5ca02fd73b2dfeee
Alex-Grh <alex_grh@mail.ru>
2022-04-24 00:16:59 +0300

    1

--- 02/js/script_1.js
+++ 02/js/script_1.js
@@ -1,3 +1,11 @@
 let a1 = 6;
 let b1 = "Hello";
 console.log(a1);
+
+let inputIn = document.querySelector(".input-in");
+let button = document.querySelector("button");
+
+button.onclick = function () {
+   // кнопка будет нажата
+   console.log("Работает!")
+}
