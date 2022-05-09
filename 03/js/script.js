8a2767989f994847cacc06ca150911a8e06b722b
Alex-Grh <alex_grh@mail.ru>
2022-05-09 22:59:09 +0400

    1

--- 03/js/script.js
+++ 03/js/script.js
@@ -30,6 +30,7 @@ button.onclick = () => {
 }
 
 // Домашнее задание
+
 // Task 1
 /*
 При нажатии кнопки b-1 срабатывает функция f1. 
@@ -37,14 +38,67 @@ button.onclick = () => {
 его с числом 4 (сравнение ==). Результат сравнения - true 
 или false выведите в out-1.
 */
-let i1 = document.querySelector(".i-1");
-let div1 = document.querySelector("oun-1");
+let i1 = document.querySelector(".i-1"); //введите число Input i-1
+let div1 = document.querySelector(".out-1"); //вывести сравнения - true или false
+let div11 = document.querySelector(".out-11"); //вывести результат число
 
 function f1() {
-if (i1 == 4){
-console.log(true);
+   let b11 = i1.value
+   div11.innerHTML = b11;
+   if (b11 == 4) {
+      div1.innerHTML = true;
+      console.log(true);
+   } else {
+      div1.innerHTML = false;
+      console.log(false);
+   }
+}
+document.querySelector(".b-1").onclick = f1; //срабатывает кнопка после клика
+
+// Task 2.
+/*
+Даны две переменные a21 и a22. При нажатии кнопки b-2, 
+запускается функция f2. Функция должна сравнить переменные 
+с помощью if else и вывести в out-2 число, которое больше. 
+Вариант равенства переменных не рассматриваем.*/
+let i21 = document.querySelector(".i-21");
+let i22 = document.querySelector(".i-22");
+let div2 = document.querySelector(".out-2"); //вывести в out-2 число, которое больше
+
+function f2() {
+let a21 = i21.value;
+let a22 = i22.value;
+if (a21 > a22 ){
+div2.innerHTML = a21;
+} else if (a21 < a22){
+div2.innerHTML = a22;
 } else {
-   console.log(false);
+   div2.innerHTML = "Значения равны!";
 }
 }
-document.querySelector(".b-1").onclick = f1;
+document.querySelector(".b-2").onclick = f2;
+
+// Task 3.
+/*
+Даны 2 input - i-31 и i-32, оба - input[type=number]. 
+При нажатии кнопки b-3 срабатывает функция f3. 
+Функция должна прочитать содержимое i-31 и i-32 в переменные 
+и сравнить их, вывести в out-3 большее число.
+Проведите самостоятельный тест работы, введите пары чисел 4 и 9, 9 и 22, 5 и 111.
+*/
+let i31 = document.querySelector(".i-31");
+let i32 = document.querySelector(".i-32");
+let div3 = document.querySelector(".out-3");
+
+function f3() {
+let a31 = i31.value;
+let a32 = i32.value;
+if (a31 > a32) {
+   div3.innerHTML = a31;
+} else if (a31 < a32){
+   div3.innerHTML = a32;
+} else {
+   div3.innerHTML = "Значения равны!";
+}
+}
+document.querySelector(".b-3").onclick = f3;
