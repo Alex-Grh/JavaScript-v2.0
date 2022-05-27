83f12b2ec36cb34c6a92640f0357a47d65bf5931
Alex-Grh <alex_grh@mail.ru>
2022-05-27 23:07:32 +0400

    1

---
+++ 05/js/script.js
@@ -0,0 +1,31 @@
+// for (let i = 0; i < 5; i++) { //бесконечный цикл (let i =0; i >= 0; i++)
+//    console.log(i);
+// }
+
+// for (let i = 5; i > 0; i--) { 
+//    console.log(i);
+// }
+
+// for (let i = 0; i < 10; i = i + 2) { 
+//    console.log(i);
+// }
+
+// Выход из цикла заранее
+
+// for (let i = 0; i < 6; i++) {
+//    if (i == 4) break;
+//    console.log(i);
+// } // выводит до 3
+
+// for (let i = 0; i < 6; i++) {
+ 
+//    console.log(i);
+//    if (i == 4) break;
+// } // выводит до 4
+
+
+let div0 = document.querySelector(".out-0");
+for (let i = 0; i < 6; i++) {
+div0.innerHTML = ('***********');
+   console.log('***********');
+}
