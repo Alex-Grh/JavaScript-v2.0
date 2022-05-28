18597830e55048ccbf5b79ef89d5af327818ad65
Alex-Grh <alex_grh@mail.ru>
2022-05-28 23:50:43 +0400

    1

--- 05/js/script.js
+++ 05/js/script.js
@@ -18,15 +18,21 @@
 // } // выводит до 3
 
 // for (let i = 0; i < 6; i++) {
- 
+
 //    console.log(i);
 //    if (i == 4) break;
 // } // выводит до 4
 
 
-let div0 = document.querySelector(".one");
-console.log(div0);
-for (let i = 0; i < 6; i++) {
+// let div0 = document.querySelector(".one");
+// console.log(div0);
+// for (let i = 0; i < 6; i++) {
 // div0.innerHTML = ('***********');
-   // console.log('***********');
+//    console.log('***********');
+// }
+
+let div = document.querySelector(".one");
+console.log(div);
+for (let i = 0; i < 6; i++) {
+
 }
