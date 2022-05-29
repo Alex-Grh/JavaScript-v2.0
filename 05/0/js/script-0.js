530851029579e5b6d55ad3bd39ed5107100b7ade
Alex-Grh <alex_grh@mail.ru>
2022-05-29 23:24:45 +0400

    1

--- 05/0/js/script-0.js
+++ 05/0/js/script-0.js
@@ -87,10 +87,12 @@ document.querySelector(".out-01").innerHTML = out01;
 // Кнопка b-1 запускает функцию t1. Функция должна выводить в out-1 строку вида:
 //     1 2 3 4 5 6 7 8 9 ... 49 50
 // от 1 до 50 включительно. Разделитель - пробел. Задача решается с помощью цикла.
-let div1 = document.querySelector(".out-1")
 
+let out1 = "";
 function t1() {
-
-
+for (let i = 0; i <= 50; i++){
+  out1 += i + " ";
+}
+document.querySelector(".out-1").innerHTML = out1;
 }
 document.querySelector(".b-1").onclick = t1;
