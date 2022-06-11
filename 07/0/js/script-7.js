5fa9f8b7dab624f7fe5bd4300316f818330996df
Alex-Grh <alex_grh@mail.ru>
2022-06-11 23:14:57 +0400

    1

--- 07/0/js/script-7.js
+++ 07/0/js/script-7.js
@@ -1,3 +1,30 @@
+// Task 0
+let f0 = document.querySelector(".f-0");
+function one() {
+console.log("Work");
+}
+f0.onclick = one;
+
+function two() {
+   console.log("work 2222");
+   return 54;
+}
+two(); 
+console.log(1 + two());
+
+let a = 8;
+let b = 9;
+function multi() {
+   console.log(a * b);
+   return a * b;
+}
+multi()
+let c1 = multi();
+let c2 = 10 * multi();
+console.log (c1, c2);
+
+
+
 // Task 1
 // Напишите функцию t1, которая при нажатии кнопки выводит в out - 1 переменную a1.
 
