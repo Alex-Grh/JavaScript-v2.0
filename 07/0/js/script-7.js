31ce39e61c75aaa33574310690f123798206a37a
Alex-Grh <alex_grh@mail.ru>
2022-06-15 12:08:59 +0400

    1

--- 07/0/js/script-7.js
+++ 07/0/js/script-7.js
@@ -1,9 +1,19 @@
 // Task 0
-let f0 = document.querySelector(".f-0");
+let f0 = document.querySelector(".f-0");//кнопка
 function one() {
 console.log("Work");
 }
-f0.onclick = one;
+// f0.onclick = one; // one без круглых скобок
+
+//Анонимная функция. У нее нет имени. Можно использовать один раз.
+f0.onclick = function () {
+   console.log("Hello");
+}
+//Аналог (один из способов) анонимной функции это стрелочная функция
+f0.onclick = () => {
+   console.log("Hello1");
+}
+
 
 function two() {
    console.log("work 2222");
@@ -23,14 +33,62 @@ let c1 = multi();
 let c2 = 10 * multi();
 console.log (c1, c2);
 
+function multi2 (x = 9, y = 8) { //можно задавать параметры по умолчанию. Срабатывают когда незаданно других значений
+   return x * y;
+   // Все что ниже return срабатывать небудет
+}
+console.log(multi2(4, 5));
+console.log(multi2(20, 5));
+console.log(multi2(20, a));
+console.log(multi2()); // срабатывают параметры по умолчанию
+console.log(multi2(10)); //х = 10 второй по умолчанию y = 8
 
+// сделать анонимную функцию и стрелочную f01
+//анонимная функция
+document.querySelector(".f-01").onclick = function(){
+   console.log("+++++++")
+};
+//стрелочная функция
+document.querySelector(".f-01").onclick = ()=> {
+   console.log("arrow")
+   console.log("+++++++")
+};
+
+// Сокращение в стрелочных функциях
+// Анонимная функция
+// function(a01, b01) {
+
+// }
+// Запишем ее стрелочной функцией
+(a01, b01) => {
+
+}
+// Запишем ее стрелочной функцией если только один параметр а можно упустить скобки
+a01 => {
+   
+}
 
 // Task 1
 // Напишите функцию t1, которая при нажатии кнопки выводит в out - 1 переменную a1.
-
+let out1 = document.querySelector(".out-1");
 let a1 = 8;
 function t1() {
-
+out1 .innerHTML = a1;
 }
 
 document.querySelector('.b-1').onclick = t1;
+
+// Task 2.
+// Изменим задачу 1. Сейчас она только выводит переменную в заранее 
+// заданный блок.Давайте сделаем так, чтобы функция, была более гибкой.
+// Пусть теперь функция t2 возвращает переменную a2.Поскольку 
+// функция возвращает переменную, то имя функции со скобками(вызов функции) 
+// можно встраивать в выражения.Обратите внимание, как изменяется 
+// вызов функции теперь.
+let a2 = 8;
+function t2() {
+   return a2
+}
+document.querySelector(".b-2").onclick = function(){
+   document.querySelector(".out-2").textContent = t2()
+}
