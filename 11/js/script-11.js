2cc3ddb09c947a5ef78251cf950984a417b36052
Alex-Grh <alex_grh@mail.ru>
2022-07-10 17:48:57 +0400

    1

--- 11/js/script-11.js
+++ 11/js/script-11.js
@@ -88,3 +88,14 @@ function f1() {
    showArr('.out-1', d1);
 }
 document.querySelector('.b-1').onclick = f1;
+
+// Task 2
+//Напишите функцию f2, которая применяет метод pop к массиву d1, а затем выводит его (showArr) 
+// в .out-2 функция выполняется при нажатии кнопки b-2
+
+function f2() {
+d1.pop();
+   showArr('.out-2', d1);
+}
+
+document.querySelector('.b-2').onclick = f2;
