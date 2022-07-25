b1229120f315c263cdb7931ae4c4e4f6e201e999
Alex-Grh <alex_grh@mail.ru>
2022-07-26 00:36:54 +0400

    1

--- 13/js/script-13.js
+++ 13/js/script-13.js
@@ -79,3 +79,20 @@ function f1() {
    return a1.two;
 }
 document.querySelector('.b-1').onclick = f1;
+
+// Task 2
+// При нажатии b-2 выполняете функцию f2. Функция должна вывести в out-2 
+// значение hi из массива a2. Возвратите это значение.
+// Вывод, как и в предыдущем задании пишем "руками"
+
+function f2() {
+   let a2 = {
+'one': 'hello',
+'two': 'mahai',
+'five': 'hi'
+   };
+return a2.five;
+}
+document.querySelector('.b-2').onclick = () => {
+   document.querySelector('.out-2').innerHTML = f2();
+}
