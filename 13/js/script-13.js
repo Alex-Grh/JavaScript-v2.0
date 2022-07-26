0e1d547a4290a3bbbc8fd9b4fd792d654e247238
Alex-Grh <alex_grh@mail.ru>
2022-07-26 17:14:30 +0400

    1

--- 13/js/script-13.js
+++ 13/js/script-13.js
@@ -96,3 +96,22 @@ return a2.five;
 document.querySelector('.b-2').onclick = () => {
    document.querySelector('.out-2').innerHTML = f2();
 }
+
+// Task 3
+// При нажатии b-3 выполняете функцию f3. Функция должна вывести в out-3 
+// значение hi из массива a3. Возвратите это значение. Вывод - через пробел.
+// Вывод, как и в предыдущем задании пишем "руками"
+function f3() {
+   let a3 = {
+'one': 'hello',
+'two': 'mahai',
+'five': 'hi',
+'test': 21,
+'odd': 'hi',
+'mix': 'mix'
+   };
+   return a3.five + ' ' + a3.odd;
+}
+document.querySelector('.b-3').onclick = () => {
+   document.querySelector('.out-3').innerHTML = f3();
+}
