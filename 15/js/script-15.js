ca6a71bcb6bab0540f25e711a2819dbc87d84151
Alex-Grh <alex_grh@mail.ru>
2022-08-14 14:24:48 +0400

    1

--- 15/js/script-15.js
+++ 15/js/script-15.js
@@ -119,3 +119,23 @@ const f5 = () => {
 }
 
 document.querySelector('.b-5').onclick = f5;
+
+// Task 6
+// При нажатии b-6 выполняете функцию f6. Функция должна вывести в out-6 число 
+// уникальных элементов в массиве a6. Решение должно использовать set.
+
+let a6 = [1, 2, 3, 4, 5, 3, 4, 5, 2, 4, 5, 3, 24, 5, 2, 4, 56, 4, 3, 2, 335, 2, 23, 41, 3, 4, 1, 1, 4, 2, 2, 4, 5, 24, 5, 3, 22, 56];
+out6 = document.querySelector('.out-6');
+const f6 = () => {
+  
+// let a61 = new Set(a6); // выводим только уникальные элементы
+// out6.innerHTML = a61;
+// console.log(a61);
+
+let a61 = Array.from(new Set(a6)) // выводим только уникальные элементы
+out6.innerHTML = a61;
+console.log(a61);
+
+ }
+
+document.querySelector('.b-6').onclick = f6;
