64e804a3d779402614a7b321dd9faacede0d2f77
Alex-Grh <alex_grh@mail.ru>
2022-08-14 12:02:54 +0400

    1

--- 15/js/script-15.js
+++ 15/js/script-15.js
@@ -91,3 +91,17 @@ const f3 = () => {
 }
 
 document.querySelector('.b-3').onclick = f3;
+
+// Task 4
+// При нажатии b-4 выполняете функцию f4. Функция должна проверить наличие 
+// в наборе s4 строки введенной пользователем в i-4. Если строка есть - то 
+// вывести в out-4 true. Если нет - false.
+
+let s4 = new Set(['a', 'b', 'c', 'z']);
+
+const f4 = () => {
+   i4 = document.querySelector('.i-4').value;
+   console.log(s4.has(i4));
+ }
+
+document.querySelector('.b-4').onclick = f4;
