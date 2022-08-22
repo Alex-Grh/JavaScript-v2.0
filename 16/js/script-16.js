d04ad0067994f73b04ffc7590a65b742b28c46cd
Alex-Grh <alex_grh@mail.ru>
2022-08-22 23:03:05 +0400

    1

--- 16/js/script-16.js
+++ 16/js/script-16.js
@@ -83,3 +83,14 @@ for (let i = 0; i < a2.length; i++) {
 }
 
 document.querySelector('.b-2').addEventListener('click', f2);
+
+// Task 3
+//При нажатии .b-3 выполняете функцию f3. Функция получает div.out-3 со 
+// страницы с помощью getElementsByClassName и в каждый записывает число 3, 
+// перезаписывая содержимое div.
+
+function f3() {
+
+}
+
+document.querySelector('.b-3').addEventListener('click', f3);
