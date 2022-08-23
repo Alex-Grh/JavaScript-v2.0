653acc0b67585bebb9901523bcc4f31807188854
Alex-Grh <alex_grh@mail.ru>
2022-08-23 23:55:17 +0400

    1

--- 16/js/script-16.js
+++ 16/js/script-16.js
@@ -99,3 +99,16 @@ console.log(item);
 }
 
 document.querySelector('.b-3').addEventListener('click', f3);
+
+// Task 4
+//При нажатии .b-4 выполняете функцию f4. Функция получает div.out-4 со 
+// страницы с помощью querySelectorAll и в каждый дописывает число 4.
+
+function f4() {
+let out4 = querySelectorAll('out-4');
+for (let item of out4) {
+   item.innerHTML = 4;
+}
+}
+
+document.querySelector('.b-4').addEventListener('click', f4);
