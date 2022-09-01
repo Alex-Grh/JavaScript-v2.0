cbcfe81a947afaa3722613f9d9b9542292a8cb1d
Alex-Grh <alex_grh@mail.ru>
2022-09-01 14:41:17 +0400

    1

--- 17/js/script-17.js
+++ 17/js/script-17.js
@@ -449,3 +449,25 @@ return b6_res;
 document.querySelector('.b-6').onclick = () => {
     console.log(t6());
 }
+
+// Task 7 ============================================
+/*  Дан массив b7 = ["php-7", "html", "css", 92, "6", "5", "hello", 32]. C помощью 
+filter переберите массив b7 и создайте  b7_res, который содержит только строки из b7, 
+длина которых больше 3. Возвратите b7_res. Действия должны запускаться при вызове 
+функции t7. */
+
+let b7 = ["php-7", "html", "css", 92, "6", "5", "hello", 32];
+
+function t7() {
+let b7_res = b7.filter(function(elem){
+   if (elem.length > 3 && typeof elem == 'string') {
+      return true; // также можно и return elem;
+   } 
+});
+return b7_res;
+}
+
+document.querySelector('.b-7').onclick = () => {
+    console.log(t7());
+}
+
