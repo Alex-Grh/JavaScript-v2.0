c34c5598074f5d2fc5d67bee5562ecb5e2bc60a1
Alex-Grh <alex_grh@mail.ru>
2022-09-15 23:15:48 +0400

    1

--- 20/js/script-20.js
+++ 20/js/script-20.js
@@ -28,10 +28,33 @@ document.querySelector('.i-02').onkeypress = function (event) {
 
 
 // onkeydown событие фиксируется когда клавиша нажата но еще неотпущена
-// срабативает при нажатии других клавиш верх, вниз, влево, вправо, контрл, шифт
+// срабативает при нажатии других клавиш верх, вниз, влево, вправо, контрл, шифт, CapsLock 
 
 document.querySelector('.i-02').onkeydown = function (event) {
    console.log('---------- onkeydown ----------');
+   console.log('charCode:' + event.charCode); // charCode - событие показывает код нажатой клавиши
+   console.log('code:' + event.code); // code - событие показывает какая клавиша нажата
+   console.log('key:' + event.key); // key - событие показывает какая клавиша нажата
+   console.log('keyCode:' + event.keyCode); // keyCode - событие показывает код нажатой клавиши
+   // console.log(event);
+   if (event.key == 'CapsLock ') {
+      document.querySelector('.ch-01').checked = true;
+   } else {
+      document.querySelector('.ch-01').checked = false;
+   }
+}
+
+событие onkeyup
+document.querySelector('.i-02').onkeydown = function (event) {
+   console.log('---------- onkeyup ----------');
+   console.log('charCode:' + event.charCode); // charCode - событие показывает код нажатой клавиши
+   console.log('code:' + event.code); // code - событие показывает какая клавиша нажата
+   console.log('key:' + event.key); // key - событие показывает какая клавиша нажата
+   console.log('keyCode:' + event.keyCode);
+}
+
+document.querySelector('.i-03').onkeypress = function (event) {
+   console.log('---------- onkeypress ----------');
    console.log('charCode:' + event.charCode); // charCode - событие показывает код нажатой клавиши
    console.log('code:' + event.code); // code - событие показывает какая клавиша нажата
    console.log('key:' + event.key); // key - событие показывает какая клавиша нажата
