c4bef6800a863da9d9655a8bde89b08910e1728d
Alex-Grh <alex_grh@mail.ru>
2022-09-25 17:20:20 +0400

    1

--- 21/js/script-21.js
+++ 21/js/script-21.js
@@ -7,27 +7,28 @@ document.querySelector('.block-2').addEventListener('touchmove', myTouchMove);//
 function myTouch(event) {
    console.log(event);
    console.log('touch');
-   document.querySelector('.out-1').innerHTML = event.touches.lenght;
-   document.querySelector('.out-2').innerHTML += 'work' + " ";
+   document.querySelector('.out-01').innerHTML = event.touches.lenght;
+   document.querySelector('.out-02').innerHTML += 'work' + " ";
 }
 
 function myTouchEnd(event) {
-    document.querySelector('.out-3').innerHTML += 'end' + " ";
+    document.querySelector('.out-03').innerHTML += 'end' + " ";
 }
 
 function myTouchMove(event) {
    event.preventDefault();
    console.log(event);
-   document.querySelector('.out-1').innerHTML = event.touches.lenght;   
-   document.querySelector('.out-3').innerHTML += 'move' + " ";
+   document.querySelector('.out-01').innerHTML = event.touches.lenght;   
+   document.querySelector('.out-03').innerHTML += 'move' + " ";
    return false;
 }
 
 //!------------------------------------------------------------------------------------
 // Task 1 ============================================
 /* Создайте блок div-1. Добавьте на него событие touchstart. Выведите в out-1 слово  touch если событие сработает. */
-
+document.querySelector('.div-1').addEventListener('touchstart', myTouch);
 function t1() {
+   document.querySelector('.out-1').innerHTML += "touch";
 }
 
 // ваше событие здесь!!!
