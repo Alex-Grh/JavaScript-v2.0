11cdeae07b3df09672652c390415fa4cc5905a79
Alex-Grh <alex_grh@mail.ru>
2022-10-10 23:54:08 +0400

    1

--- 22/js/script-22.js
+++ 22/js/script-22.js
@@ -27,28 +27,36 @@ function t1() {
 function t1() {
    // тут добавляете try
    try {
-   let a = 22;
-   let c = a + d;
-   document.querySelector('.out-1').innerHTML = с;
+      let a = 22;
+      let c = a + d;
+      document.querySelector('.out-1').innerHTML = с;
    }
-   catch (err){
-   // тут catch
-   // .. и вывод
-   document.querySelector('.out-1').innerHTML = 12; 
-   console.log('Ошибка');
+   catch (err) {
+      // тут catch
+      // .. и вывод
+      document.querySelector('.out-1').innerHTML = 12;
+      console.log('Ошибка');
    }
 }
 
 document.querySelector('.b-1').onclick = t1;
 
 //  Task 2
-// Добавьте в код функции try catch так, чтобы вместо ошибки был вывод результата в out-2.
+// Добавьте в код функции try catch так, чтобы вместо ошибки был вывод результата 
+// в out-2.
 
 function t2() {
-   let a = 4;
-   let b = 5;
-   document.querySelector('.out-2222222').innerHTML = a*b;
-
+   try {
+      let a = 4;
+      let b = 5;
+      document.querySelector('.out-2222222').innerHTML = a * b;
+   }
+   catch (err) {
+      // тут catch
+      // .. и вывод
+      document.querySelector('.out-2').innerHTML = a * b;
+      console.log('Ошибка');
+   }
 }
 
 document.querySelector('.b-2').onclick = t2;
@@ -60,7 +68,7 @@ document.querySelector('.b-2').onclick = t2;
 function t3() {
    let a = 4;
    let b = 5;
-   document.querySelector('.out-3').innerHTML = a*b;
+   document.querySelector('.out-3').innerHTML = a * b;
 }
 
 document.querySelector('.b-3').onclick = t3;
