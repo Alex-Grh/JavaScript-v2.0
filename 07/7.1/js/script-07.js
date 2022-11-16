971a243e9049598b3bb6442cd2f0c02ef8d0d146
Alex-Grh <alex_grh@mail.ru>
2022-11-17 00:52:04 +0400

    1

--- 07/7.1/js/script-07.js
+++ 07/7.1/js/script-07.js
@@ -1,3 +1,5 @@
+//!Именнованные функции
+
 let b01 = document.querySelector('.b-01'); //кнопка
 
 function one() {
@@ -5,15 +7,83 @@ function one() {
 }
 one();
 
-b01.onclick = one;
+// b01.onclick = one; //без круглых скобок
+
+console.log(1 + one());
+console.log(one()); // underfined
+
+function two() {
+   console.log('work 2222');
+   return 54; // метка функции прекратить работу
+}
+
+two();
+console.log(1 + two());
+
+let a = 8;
+let b = 9;
+let c;
+
+function multi() {
+   console.log(a + b);
+   // c = a * b;
+   return a * b;
+}
+// multi();
+// console.log(c);
+
+let c1 = multi();
+let c2 = 10 * multi();
+console.log(c1, c2);
 
 
+console.log('Вводим параметры функции---------------------------------------------------------');
+//Вводим параметры функции. Пререпишем умножение с помощью параметров.
+// Параметры это теже переменныее водимые в нутри функции.
+// Функция стала универсальной
+// Функция это способ повторного использования кода
 
 
+// function multi2(x, y) {
+//    return x * y;
+// }
+// console.log(multi2(4, 5));
+// console.log(multi2(20, 5));
+// console.log(multi2(20, a));
 
+// Можно использовать авраметры по умолчанию
 
+function multi2(x = 9, y = 8) {
+   return x * y;
+   // все что после return выполнятся небудет
+}
+console.log(multi2(4, 5));
+console.log(multi2(20, 5));
+console.log(multi2(20, a));
+// Если параметры непередвать то запишутся параметры по умолчанию
+console.log(multi2()); //9 * 8 = 72
+console.log(multi2(10)); //если передать один параметр то заменить х и будет 10 * 8 = 80
 
+//! Анонимные функции
+// Вызываются только один раз
 
+// b01.onclick = function () {
+//    console.log('Hello');
+// };
+
+//! Стрелочная функция. Это один из способов написания анонимных функций
+
+b01.onclick = () => {
+   console.log('Hello');
+};
+
+// Если написать две анонимных или стрелочных функций то сработает последняя (Вызываются только один раз)
+// Если надо написать две и более функции то необходимо использовать именную функцию
+
+document.querySelector('.b-02').onclick = () => {
+   console.log('arrow');
+   console.log('+++++++++');
+}
 
 
 
