a02cd3b3219e4548161c46d52efdb14ed2b17744
Alex-Grh <alex_grh@mail.ru>
2022-12-26 03:52:30 +0400

    1

--- 14/14.1/js/script-14.1.js
+++ 14/14.1/js/script-14.1.js
@@ -13,3 +13,29 @@ fetch('http://api.openweathermap.org/data/2.5/weather?id=703448&appid=70e1ed322b
         // catch any errors
     });
 */
+/*
+{
+        "id": 3245,
+        "name": "Taglag",
+        "state": "",
+        "country": "IR",
+        "coord": {
+            "lon": 44.98333,
+            "lat": 38.450001
+        }
+    },
+*/
+
+
+fetch('http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=79f2180f696f74f48844082da9bb130b')
+   .then(function (resp) { return resp.json() }) // Convert data to json
+   .then(function (data) {
+      console.log(data);
+      document.querySelector('.package-name').textContent = data.name;
+      document.querySelector('.price').innerHTML = Math.round(data.main.temp - 273) + '&deg;' + 'C';
+      document.querySelector('.disclaimer').textContent = data.weather[0]['description'];
+      document.querySelector('.features li').innerHTML = `<img src="https://openweathermap.org/img/wn/${data.weather[0]['icon']}@2x.png">`;
+   })
+   .catch(function () {
+      // catch any errors
+   });
